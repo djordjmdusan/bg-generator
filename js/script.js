c4a75f52362fb4041092ee06b297d63a08a91360
Djordjevic Dusan <75639579+djordjmdusan@users.noreply.github.com>
2020-12-21 21:40:15 +0100

    Bg-generator app upload

---
+++ js/script.js
@@ -0,0 +1,24 @@
+let css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.querySelector("body");
+var btnRandom = document.querySelector(".randomGradient");
+
+setGradient();
+
+function setGradient() {
+  body.style.background =
+    "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
+
+  css.textContent = body.style.background + ";";
+}
+
+color1.addEventListener("input", setGradient);
+
+color2.addEventListener("input", setGradient);
+
+btnRandom.addEventListener("click", function () {
+  color1.value = "#" + Math.floor(Math.random() * 16777215).toString(16);
+  color2.value = "#" + Math.floor(Math.random() * 16777215).toString(16);
+  setGradient();
+});
